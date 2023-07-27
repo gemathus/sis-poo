c23fc70c44273102a8c5302f056c1fb8080f9c68
Gerardo Mathus <gerardo@nextia.mx>
2023-07-26 20:19:27 -0600

    add student class

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, redirect
+from entities.student import Student
 
 app = Flask(__name__)
 
 students = []
 
+
 @app.route("/")
 def index():
-    return render_template('index.html', students=students)
+    return render_template('index.html', students_list=students)
 
 
 @app.route("/new_student", methods=['GET', 'POST'])
@@ -15,15 +17,16 @@ def new_student():
         print("Método GET")
         return render_template('new_student.html')
     elif request.method == "POST":
-        estudiante = {
-            "nombre": request.form["nombre"],
-            "apellido_paterno": request.form["apellido_paterno"],
-            "apellido_materno": request.form["apellido_materno"],
-            "fecha_nacimiento": request.form["fecha_nacimiento"],
-            "matricula": request.form["matricula"]
-        }
+        estudiante = Student(
+            request.form["nombre"],
+            request.form["apellido_paterno"],
+            request.form["apellido_materno"],
+            request.form["fecha_nacimiento"],
+            request.form["matricula"]
+        )
         students.append(estudiante)
         return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8888)
---
+++ entities/student.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, nombre, ap_paterno, ap_materno, fech_nacimiento, matr):
+        self.nombre = nombre
+        self.apellido_paterno = ap_paterno
+        self.apellido_materno = ap_materno
+        self.fecha_nacimiento = fech_nacimiento
+        self.matricula = matr
